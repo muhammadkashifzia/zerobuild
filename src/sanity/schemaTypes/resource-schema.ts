1f25fd71d3780dab397d94c932a748eee9f7d40a
kashifidenbrid <eastlogic.kashif@gmail.com>
2025-07-26 16:33:40 +0500

    Update resource-schema.ts

--- src/sanity/schemaTypes/resource-schema.ts
+++ src/sanity/schemaTypes/resource-schema.ts
@@ -6,34 +6,7 @@ export const resourceType = defineType({
   title: 'Resource',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-      validation: (rule) => rule.required(),
-    }),
-   
-    defineField({
-      name: 'publishedAt',
-      title: 'Published At',
-      type: 'datetime',
-      initialValue: () => new Date().toISOString(),
-      validation: (rule) => rule.required(),
-    }),
-    defineField({
-      name: 'image',
-      title: 'Icon Image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'description',
-      title: 'Short Description',
-      type: 'string',
-      validation: (rule) => rule.max(300),
-    }),  defineField({
+     defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
