70bdf52bdae3e2ee049c5e0be40d5bd7de6cb44d
kashifidenbrid <eastlogic.kashif@gmail.com>
2025-07-26 16:04:11 +0500

    Update resource-schema.ts

--- src/sanity/schemaTypes/resource-schema.ts
+++ src/sanity/schemaTypes/resource-schema.ts
@@ -33,7 +33,57 @@ export const resourceType = defineType({
       title: 'Short Description',
       type: 'string',
       validation: (rule) => rule.max(300),
+    }),  defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (rule) => rule.required(),
     }),
-   
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'title' },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published At',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'description',
+      title: 'Short Description',
+      type: 'string',
+      validation: (rule) => rule.max(300),
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{ type: 'string' }],
+    }),
+    defineField({
+      name: 'gallery',
+      title: 'Gallery',
+      type: 'array',
+      of: [{ type: 'image' }],
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body Content',
+      type: 'array',
+      of: [{ type: 'block' }],
+})
   ],
 })
