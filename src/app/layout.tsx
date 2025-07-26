5eb8087354a4d33266306090629cdac9ba143e6d
kashifidenbrid <eastlogic.kashif@gmail.com>
2025-07-26 14:02:02 +0500

    jh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
+
 const NotoSans = Noto_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
